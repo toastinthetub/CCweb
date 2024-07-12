340d3e411a736cda3ec802e6e26bc4302468a191
toastinthetub <johnapplebiz@gmail.com>
2024-07-11 20:05:38 -0700

    update Cargo.toml

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,12 @@
+use crossterm::{
+    cursor::MoveTo,
+    terminal::{self, Clear, ClearType},
+    QueueableCommand,
+};
+
+use reqwest::Client;
+use std::error::Error;
+
 fn main() {
     println!("Hello, world!");
 }
